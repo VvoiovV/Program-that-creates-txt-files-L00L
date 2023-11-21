3c02b88bacfa6c0e18a76d374b7d3046b34d7aed
Michał Grzegorzewski <grzegorzgrzegolak@gmail.com>
2023-11-21 22:44:06 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            for (int i = 1; i < 10; i++)
+            {
+                string filePath = $"L0{new string('0', i)}L.txt";
+                CreateTextFile(filePath);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Błąd: " + ex.Message);
+        }
+    }
+
+    static void CreateTextFile(string filePath)
+    {
+        using (StreamWriter sw = new StreamWriter(filePath))
+        {
+            for (int i = 0; i < 200; i++)
+            {
+                for (int j = 0; j < 200; j++)
+                {
+                    sw.Write("L0000000000L ");
+                }
+                sw.WriteLine();
+            }
+        }
+    }
+}
